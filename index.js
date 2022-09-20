b860036506ab2c69d848ba0c86ee31109f16b4d6
act1313 <act3313@gmail.com>
2022-09-20 16:24:54 -0400

    finished adding the question will make interactive

---
+++ index.js
@@ -0,0 +1,20 @@
+const questions = ["Where were you and what were you doing when 9/11 happened?", 
+    "What was your reaction to it?", 
+    "What was it like to be in D.C. and a plane crash into the Pentagon?",
+    "What was your reaction to President Bush\'s speech?",
+    "What was the aftermath like (the weeks after the initial attack)?",
+    "How long did it take for things to get back to normal?",
+    "How has this changed your life?",
+    "Do you think America has grown stronger as a country because of 9/11?"];
+
+const answers = ["I was sitting in my apartment in Washington, D.C., watching the today show and saw the first plane hit the World Trade Center.",
+    "I was confused and scared because I lived by myself and I didn\'t know what was happening. I had a class that morning so I walked to the school and there were notices that said class was canceled. I walked back to my apartment and watched the news all day. For most of the day, phone lines were down, so I couldn\'t contact anyone and no one could contact me. By the end of the day I was able to contact my mom. That was scary for both me and my parents because we couldn't get in touch with each other for a while.",
+    "It was crazy, from what I can remember most things were shut down for a few days. My school was shut down, where I worked at, the Department of Justice, was shut down, and there weren\'t a lot of things going on in the city because so many places were closed. About three or four days after the incident, my friend and her husband took me out to dinner. We did that because we needed to do something normal.",
+    "I\'m sure I saw his speech since I was binge watching the news. But I don\'t specifically remember it.",
+    "It was crazy. The biggest change was armed guards at various places. They were definitely at my work. That\'s when they started having armed guards at the airport, as well. I don\'t specifically recall any changes to my school, but places like national monuments and museums had armed guards.",
+    "I would say there is a new normal, but I wasn\'t in D.C. for that long after because I graduated from law school and moved to Boston in December.",
+    "The only thing that I have seen that changed is the airport security.",
+    "Before the event happened, I had never wanted to move back close to my family. After the event, I actually tried to get a job back in PIttsburgh even though I had a job lined up in Boston. That didn\'t work out, though. It did make me second guess not wanting to move back home. I finally did move back home after several years.",
+    "At the time yes. At the time, I felt the country was very united because something we thought would never happen, happened on our soil. I never thought I\'d feel as scared again, but some of the same feelings occurred when the Boston Marathon bombing happened. Looking at the country today though, I feel we are less united even before the 9/11 attacks."];
+
+console.log(answers[3]);
